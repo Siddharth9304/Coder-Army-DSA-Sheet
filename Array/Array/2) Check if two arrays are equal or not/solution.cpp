91933186811f78cb332d3b3c2d30070bcd60094d
Siddharth9304 <siddharthprem9304@gmail.com>
2024-10-28 01:59:15 +0530

    Update solution.cpp

--- Array/Array/2) Check if two arrays are equal or not/solution.cpp
+++ Array/Array/2) Check if two arrays are equal or not/solution.cpp
@@ -1,1 +1,9 @@
 //Dificulty Level - Easy
+//Problem Statement:
+//Given two arrays arr1 and arr2 of equal size, the task is to find whether the given arrays are equal. Two arrays are said to be equal if both contain the same set of elements, arrangements (or permutations) of elements may be different though.
+//Note: If there are repetitions, then counts of repeated elements must also be the same for two arrays to be equal.
+
+//Expected Time Complexity: O(n)
+//Expected Space Complexity: O(n)
+
+
